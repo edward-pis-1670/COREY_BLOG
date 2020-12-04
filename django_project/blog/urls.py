55698d27c0fa7a8ae4c43ed21aca60a229eb1cb5
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2020-12-04 16:57:38 +0700

    final

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -16,5 +16,4 @@ urlpatterns=[
     path('post/new/', PostCreateView.as_view(), name= 'post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name= 'post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name= 'post_delete'),
-
 ]
